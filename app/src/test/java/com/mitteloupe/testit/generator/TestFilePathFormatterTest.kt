f6a946f692307011ca58494c43bb6d62b8dff622
Eran Boudjnah <eranbou@gmail.com>
2019-07-30 22:23:34 +0100

    Added test coverage for TestFilePathFormatter.

---
+++ app/src/test/java/com/mitteloupe/testit/generator/TestFilePathFormatterTest.kt
@@ -0,0 +1,41 @@
+package com.mitteloupe.testit.generator
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Before
+import org.junit.Test
+
+class TestFilePathFormatterTest {
+    private lateinit var cut: TestFilePathFormatter
+
+    @Before
+    fun setUp() {
+        cut = TestFilePathFormatter()
+    }
+
+    @Test
+    fun `Given source file name when getTestFilePath then returns expected path for test`() {
+        // Given
+        val basePath = "/Users/me/Projects/TestIt/app/src"
+        val sourceFileName = "$basePath/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt"
+        val expected = "$basePath/test/java/com/mitteloupe/testit/generator/"
+
+        // When
+        val actual = cut.getTestFilePath(sourceFileName)
+
+        // Then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Given unrecognized file name when getTestFilePath then returns null`() {
+        // Given
+        val sourceFileName = "/Users/me/Projects/TestIt/app/source/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt"
+
+        // When
+        val actual = cut.getTestFilePath(sourceFileName)
+
+        // Then
+        assertNull(actual)
+    }
+}
