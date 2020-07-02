53ed43bdd21abbc242efe45feae14e1f4d870510
Eran Boudjnah <eranbou@gmail.com>
2020-07-02 18:50:19 +0100

    Added support for Windows file paths.

--- app/src/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt
+++ app/src/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt
@@ -1,13 +1,19 @@
 package com.mitteloupe.testit.generator
 
+import java.io.File
+
 class TestFilePathFormatter {
-    private val pathRegex by lazy { Regex("/src/[a-z]+/(java|kotlin)/") }
+    private val pathRegex by lazy {
+        val separator = Regex.escape(File.separator)
+        Regex("${separator}src${separator}[a-z]+${separator}(java|kotlin)$separator")
+    }
 
     fun getTestFilePath(sourceFileName: String) =
         if (pathRegex.containsMatchIn(sourceFileName)) {
-            val sourcePath = sourceFileName.substringBeforeLast("/") + "/"
+            val separator = File.separator
+            val sourcePath = sourceFileName.substringBeforeLast(separator) + separator
             pathRegex.replace(sourcePath) { matchResult ->
-                "/src/test/${matchResult.groupValues[1]}/"
+                "${separator}src${separator}test${separator}${matchResult.groupValues[1]}$separator"
             }
         } else {
             null
