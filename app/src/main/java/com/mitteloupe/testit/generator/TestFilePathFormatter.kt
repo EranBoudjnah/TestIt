48f3d82ced94a88456dcd2e7d552768d93231922
Eran Boudjnah <eranbou@gmail.com>
2020-09-16 15:48:29 +0100

    Fixed path parsing.

--- app/src/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt
+++ app/src/main/java/com/mitteloupe/testit/generator/TestFilePathFormatter.kt
@@ -7,14 +7,14 @@ class TestFilePathFormatter(
 ) {
     private val pathRegex by lazy {
         val separator = Regex.escape(fileSeparator)
-        Regex("($separator)src\\1[a-zA-Z]+\\1(?:java|kotlin)\\1")
+        Regex("($separator)src\\1[a-zA-Z]+\\1(java|kotlin)\\1")
     }
 
     fun getTestFilePath(sourceFileName: String) =
         if (pathRegex.containsMatchIn(sourceFileName)) {
             val sourcePath = sourceFileName.substringBeforeLast(fileSeparator) + fileSeparator
             pathRegex.replace(sourcePath) { matchResult ->
-                "${fileSeparator}src${fileSeparator}test$fileSeparator${matchResult.groupValues[1]}$fileSeparator"
+                "${fileSeparator}src${fileSeparator}test$fileSeparator${matchResult.groupValues[2]}$fileSeparator"
             }
         } else {
             null
