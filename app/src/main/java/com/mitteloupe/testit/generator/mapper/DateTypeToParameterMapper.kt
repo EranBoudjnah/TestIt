590b59c2280627b1757145173654c676ee0b6b16
Eran Boudjnah <eranbou@gmail.com>
2020-12-11 19:56:48 +0000

    Minor tidy up.

--- app/src/main/java/com/mitteloupe/testit/generator/mapper/DateTypeToParameterMapper.kt
+++ app/src/main/java/com/mitteloupe/testit/generator/mapper/DateTypeToParameterMapper.kt
@@ -4,9 +4,7 @@ import com.mitteloupe.testit.model.DataType
 import com.mitteloupe.testit.model.TypedParameter
 
 class DateTypeToParameterMapper {
-    fun toParameters(dataTypes: List<DataType>) =
-        dataTypes.map { dataType -> toParameter(dataType) }
+    fun toParameters(dataTypes: List<DataType>) = dataTypes.map(::toParameter)
 
-    private fun toParameter(dataType: DataType) =
-        TypedParameter(dataType.name, dataType)
+    private fun toParameter(dataType: DataType) = TypedParameter(dataType.name, dataType)
 }
